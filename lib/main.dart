f354511903e5fb0a66f37421ab68c42c2d0d8050
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-08-25 14:11:07 +0300

    Added the ability to navigate using tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/tabs_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
           canvasColor: const Color.fromRGBO(255, 254, 229, 1)),
       initialRoute: '/',
       routes: {
-        '/': (context) => CategoriesScreen(),
+        '/': (context) => TabsScreen(),
         CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (context) => MealDetailScreen()
       },
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -7,22 +7,17 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple,
-        title: const Text("Meals App"),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-        children: DUMMY_CATEGORIES
-            .map((catData) => CategoryItem(catData.id, catData.title, catData.color))
-            .toList(),
-      ),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      children: DUMMY_CATEGORIES
+          .map((catData) =>
+              CategoryItem(catData.id, catData.title, catData.color))
+          .toList(),
     );
   }
 }
